c58d93221f8947ad1d5b4faecc71adefa17ed6ad
Leon <leonsunwl@gmail.com>
2023-03-31 13:05:29 +0800

    fixed automating testing workflow

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -2,25 +2,24 @@ from contextlib import contextmanager
 from time import sleep
 
 import pytest
-from playwright.sync_api import Page, expect
+# from playwright.sync_api import Page, expect
 
 LOCAL_TEST = False
 
 PORT = "8503" if LOCAL_TEST else "8699"
 
 @pytest.fixture(scope="module", autouse=True)
-def before_module():
+def test_before_module():
     # Run the streamlit app before each module
-    with run_streamlit():
+    with test_run_streamlit():
         yield
 
-@pytest.fixture(scope="function", autouse=True)
-def before_test(page: Page):
-    page.goto(f"localhost:{PORT}")
-
+# @pytest.fixture(scope="function", autouse=True)
+# def test_before(page: Page):
+#     page.goto(f"localhost:{PORT}")
 
 @contextmanager
-def run_streamlit():
+def test_run_streamlit():
     """Run the streamlit app at main.py on port 8699"""
     import subprocess
 
